8a208f5196c50dde4c4fbab02b38a1475c681d6e
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-09-27 12:39:26 +0545

    delete interaction commands

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 creator
     .withServer(new ExpressServer(app, { alreadyListening: true }))
     .registerCommandsIn(`${__dirname}/routes/slash-command`)
-    .syncCommands({ deleteCommands: true });
+    .syncCommandsIn("826739682230272002", true);
 
 app.all("*", (req, res) => {
     res.status(404).json({ error: "unknown route" });
