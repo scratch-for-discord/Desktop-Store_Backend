d1e0e87de38292a76079596efb4ff63e90c937df
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-09-27 12:51:07 +0545

    interaction commands

--- routes/slash-command/TokenCommand.js
+++ routes/slash-command/TokenCommand.js
@@ -1,11 +1,20 @@
-const { SlashCommand, CommandContext } = require("slash-create");
+const { SlashCommand, CommandContext, CommandOptionType } = require("slash-create");
 const AuthorModel = require("../../database/models/Author");
+const generateToken = require("../../utils/generateToken");
 
 class TokenCommand extends SlashCommand {
     constructor(creator) {
         super(creator, {
             name: "token",
-            description: "Get your store access token"
+            description: "Get your store access token",
+            options: [
+                {
+                    name: "regenerate",
+                    type: CommandOptionType.BOOLEAN,
+                    description: "If the token should be regenerated",
+                    required: true
+                }
+            ]
         });
         this.filePath = __filename;
     }
@@ -16,13 +25,23 @@ class TokenCommand extends SlashCommand {
     async run(ctx) {
         await ctx.defer(true);
         const user = ctx.user.id;
-        let message = "";
         const existing = await AuthorModel.findOne({ id: user });
-        if (!existing) message = "❌ | You are not registered as a block publisher!";
-        else message = `✅ | Your access token is ||${existing.accessToken}|| (Make sure to keep this private)`;
-        await ctx.sendFollowUp(message, {
-            ephemeral: true
-        });
+        if (!existing) {
+            return await ctx.sendFollowUp("❌ | You are not registered as a block publisher!", { ephemeral: true });
+        }
+        if (!ctx.options.regenerate) {
+            return await ctx.sendFollowUp(`✅ | Your access token is ||${existing.accessToken}|| (Make sure to keep this private)`, {
+                ephemeral: true
+            });
+        }
+        if (ctx.options.regenerate) {
+            existing.accessToken = generateToken();
+            await existing.save();
+
+            await ctx.sendFollowUp(`✅ | Your new access token is ||${existing.accessToken}|| (Make sure to keep this private)`, {
+                ephemeral: true
+            });
+        }
     }
 }
 
